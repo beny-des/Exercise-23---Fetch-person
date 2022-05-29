dc72c023c9abe162e54cb0cfcfadb59c34647a78
Beny Desta <benydesta@gmail.com>
2022-05-29 14:06:22 +0300

    Fetch-model

--- main23.js
+++ main23.js
@@ -1,44 +1,41 @@
-const showDetails = document.querySelector('.show-details');
-const detailsBlock = document.querySelector('.details-block');
+const showDetails = document.querySelector(".show-details");
+const detailsBlock = document.querySelector(".details-block");
 
-const res =fetch("https://randomuser.me/api/");
-
-
-
-
-showDetails.onclick=function(){ 
-    res
-    .then(function(analysisRes){
-        return analysisRes.json();
+showDetails.onclick = function () {
+  const res = fetch("https://randomuser.me/api/");
+  res
+    .then(function (analysisRes) {
+      return analysisRes.json();
     })
-    .then(function(response){
-    
-    
-const title= response.results[0].name.title;
-const firstName= response.results[0].name.first;
-const lastName= response.results[0].name.last;
-const userName = document.createElement('p');
-userName.textContent="Full Name: "+title+" "+firstName+" "+lastName
-detailsBlock.append(userName);
-userName.classList.add("info-box")
+    .then(function (response) {
+      const results = response.results[0];
+      const title = results.name.title;
+      const firstName = results.name.first;
+      const lastName = results.name.last;
+      const email = results.email;
+      const pic = results.picture.large;
+      displayUser(title, firstName, lastName, email, pic);
+    })
+    .catch(function (response) {
+      console.log("ERROR");
+    });
+};
 
-const email= response.results[0].email;
-const userEmail = document.createElement('p');
-userEmail.textContent="Email: "+email;
-detailsBlock.append(userEmail);
-userEmail.classList.add("info-box")
+function displayUser(title, firstName, lastName, email, pic) {
+  detailsBlock.innerHTML = "";
 
-const pic= response.results[0].picture.large;
-const userPic= document.createElement('img');
-userPic.src=pic;
-detailsBlock.append(userPic);
-userPic.classList.add("info-box")
-   
-    
-})
-.catch(function(response){
-    console.log("ERROR");
-    window.location.reload(true);
-})
+  const userName = document.createElement("p");
+  userName.textContent = `Full Name: ${title} ${firstName} ${lastName}`;
+  detailsBlock.append(userName);
+  userName.classList.add("info-box");
 
+  const userEmail = document.createElement("p");
+  userEmail.textContent = "Email: " + email;
+  detailsBlock.append(userEmail);
+  userEmail.classList.add("info-box");
+
+  const userPic = document.createElement("img");
+  userPic.src = pic;
+  detailsBlock.append(userPic);
+  userPic.classList.add("info-box");
 }
